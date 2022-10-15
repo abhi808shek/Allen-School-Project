bc9023a186e49becd59e35330a69ee67fc577e7d
Abhishek <abhi808gupta@gmail.com>
2022-10-15 19:37:21 +0530

    Url Updated

--- src/Components/Bca/Bca.jsx
+++ src/Components/Bca/Bca.jsx
@@ -1,76 +1,191 @@
 import React from 'react'
 import "./style/bca.css"
-
+import { a } from "react-router-dom"
 
 const Bca = () => {
-  return (
-    <div className="bca_container">
-        <div className="bca_container1">
-            <div className="bca_card1">
-                <div className="bca_main_head">
-                    <h2 className="bca_main_heading">BCA</h2>
-                </div>
-            </div>
-            <div className="bca_container2">
-                <div className="cards1 card">
-                    <div className="card_heading">
-                        1. Computer Fundamental & Problem Solving Techniques <hr/><br/>
-                        2. C Programming <hr/><br/>
-                        3. Principle of Management <hr/><br/>
-                        4. Business Communication <hr/><br/>
-                        5. Mathematics - l <hr/><br/>
+    return (
+        <div className="bca_container">
+            <div className="bca_container1">
+                <div className="bca_card1">
+                    <div className="bca_main_head">
+                        <h2 className="bca_main_heading">BCA</h2>
                     </div>
                 </div>
-                <div className="cards2 card">
-                    <div className="card_heading">
-                        1. Object Oriented Programming Using C++ <hr/><br/>
-                        2.  Internal Technology and Web Design <hr/><br/>
-                        3. Organization Behaviour <hr/><br/>
-                        4. Financial Accounting & Management <hr/><br/>
-                        5. Mathematics - || <hr/><br/>
+                <div className="bca_container2">
+                    <div className="cards1 card">
+                        <div className="card_heading">
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                1. Computer Fundamental & Problem Solving Techniques
+                                <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                2. C Programming <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                3. Principle of Management <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                4. Business Communication <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                5. Mathematics - l  <hr />
+                            </a>
+                            <br />
+
+                        </div>
                     </div>
-                </div>
-                <div className="cards3 card">
-                    <div className="card_heading">
-                        1. Python Programming <hr/><br/>
-                        2. Data Structure Using C & C++ <hr/><br/>
-                        3. Operating System <hr/><br/>
-                        4. Digital Electronics & Computer Organization <hr/><br/>
-                        5. Mathematics - 2 <hr/><br/>
+                    <div className="cards2 card">
+
+                        <div className="card_heading">
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                1. Object Oriented Programming Using C++
+                                <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                2.  Internal Technology and Web Design<hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                3. Organization Behaviour  <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                4. Financial Accounting & Management<hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                5. Mathematics - ||<hr />
+                            </a>
+                            <br />
+
+                        </div>
+
+
                     </div>
-                </div>
-                <div className="cards4 card">
-                    <div className="card_heading">
-                        1. Computer Graphics & Animation <hr/><br/>
-                        2. Database Management System <hr/><br/>
-                        3. Software Engineering <hr/><br/>
-                        4. Optimization Technique <hr/><br/>
-                        5. Mathematics - ||| <hr/><br/>
+                    <div className="cards3 card">
+
+                        <div className="card_heading">
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                1. Python Programming
+                                <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                2. Data Structure Using C & C++<hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                3. Operating System  <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                4. Digital Electronics & Computer Organization<hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                5. Mathematics - 2 <hr />
+                            </a>
+                            <br />
+
+                        </div>
+
                     </div>
-                </div>
-                <div className="cards5 card">
-                    <div className="card_heading">
-                        1. Java Programming and Dynamic Webpage Design <hr/><br/>
-                        2. Knowledge Management <hr/><br/>
-                        3. Computer Network <hr/><br/>
-                        4. Numerical Methods <hr/><br/>
-                        5. Minor Project <hr/><br/>
+                    <div className="cards4 card">
+
+                        <div className="card_heading">
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                1. Computer Graphics & Animation
+                                <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                2. Database Management System <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                3. Software Engineering <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                4. Optimization Technique <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                5. Mathematics - ||| <hr />
+                            </a>
+                            <br />
+
+                        </div>
+                      
                     </div>
-                </div>
-                <div className="cards6 card">
-                    <div className="card_heading">
-                        1. Information & Cyber Security <hr/><br/>
-                        2. Internet of Things <hr/><br/>
-                        3. E-commerce <hr/><br/>
-                        4. Data Science and Machine Learning <hr/><br/>
-                        5. Major Project <hr/><br/>
+                    <div className="cards5 card">
+
+
+                        <div className="card_heading">
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                1. Java Programming and Dynamic Webpage Design
+                                <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                2. Knowledge Management  <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                3. Computer Network<hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                4. Numerical Methods <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                5. Minor Project<hr />
+                            </a>
+                            <br />
+
+                        </div>
+
+                    </div>
+
+
+                    <div className="cards6 card">
+
+                        <div className="card_heading">
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                1. Information & Cyber Security
+                                <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                2. Internet of Things <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                3. E-commerce <hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                4. Data Science and Machine Learning<hr />
+                            </a>
+                            <br />
+                            <a href="https://www.javatpoint.com/computer-fundamentals-tuhrefrial">
+                                5. Major Project<hr />
+                            </a>
+                            <br />
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    
-  )
+
+    )
 }
 
 export default Bca;
